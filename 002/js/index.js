fa8d4a4c793679dbde19c2c56742ceed3c4a15e8
Sriyans Reddy <ysriyansreddy@gmail.com>
2021-06-17 22:11:09 +0530

    day 2

---
+++ 002/js/index.js
@@ -0,0 +1,6 @@
+const checkoutBtn = document.getElementById("checkout-btn")
+const result = document.getElementById("result")
+
+checkoutBtn.addEventListener("click", function() {
+    result.textContent = "Thank you for shopping with us."
+})
